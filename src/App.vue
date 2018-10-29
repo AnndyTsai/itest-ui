2be0142d5e29da1a2839c6728a28476f9f885656
ab.xie <ab.xie@sunvalley.com.cn>
2018-10-29 16:49:49 +0800

    初始化为vue项目

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    };
+</script>
+
+<style lang="scss">
+    @import "style/main";
+    html, body{
+        @include margin-tb-rl(0, 0);
+        @include padding-tb-rl(0, 0);
+        @include width-height(100%, 100%);
+        color: rgba(204,255,232,0.85)!important;
+    }
+    #app {
+        display: flex;
+        flex-direction: column;
+        @include margin-tb-rl(0, 0);
+        @include padding-tb-rl(0, 0);
+        @include width-height(100%, 100%);
+        font-family: Calibri;
+        text-align: center;
+        font-size: 16px;
+        background: url("assets/bg.jpg");
+       background-size: 100% 100%;
+    }
+</style>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="login-form">
+        <el-form :model="user" :rules="loginRules" status-icon ref="loginForm" label-width="1rem" class="x-el-form">
+            <el-form-item label=" " prop="userName" class="x-el-item">
+                <el-input v-model="user.userName" placeholder="Please input your account name">
+                    <div slot="prepend" class="fa fa-user-circle fa-lg" style="width: 1rem;"></div>
+                </el-input>
+            </el-form-item>
+            <el-form-item label=" " prop="password" class="x-el-item">
+                <el-input v-model="user.password" placeholder="Please input your account password">
+                    <div slot="prepend" class="fa fa-lock fa-2x" style="width: 1rem;"></div>
+                </el-input>
+            </el-form-item>
+            <el-form-item label=" " prop="captcha" class="x-el-item">
+                <el-input v-model="user.captcha" placeholder="Please input the captcha">
+                    <div slot="prepend" class="fa fa-sign-language fa-lg" style="width: 1rem;"></div>
+                </el-input>
+            </el-form-item>
+            <el-form-item></el-form-item>
+            <el-form-item>
+                <el-button type="primary">Login</el-button>
+                <el-button type="info">Register</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            var checkUserName = (rule, value, callback) => {
+                if(value === '' || value === undefined){
+                    callback('Account name cannot be empty!');
+                }
+            };
+            var checkPassword = (rule, value, callback) => {
+                if(value === '' || value === undefined){
+                    callback('Password cannot be empty!');
+                }
+            };
+            var checkCaptcha = (rule, value, callback) => {
+                if(value === '' || value === undefined){
+                    callback('Captcha cannot be empty!');
+                }
+            };
+            return {
+                user: {
+                    userName: '',
+                    password: '',
+                    captcha: '',
+                    role: '',
+                },
+                loginRules: {
+                    userName : [
+                        {validator : checkUserName, required: true, trigger : 'blur'}
+                    ],
+                    password : [
+                        {validator : checkPassword, required: true, trigger : 'blur'}
+                    ],
+                    captcha : [
+                        {validator : checkCaptcha, required: true, trigger : 'blur'}
+                    ]
+                },
+            };
+        },
+    };
+</script>
+
+<style lang="scss">
+    @import "../../style/main";
+    *{@include user-select;}
+    .login-form {
+        @include width-height(35rem, 30rem);
+        @include margin-tb-rl(10%, calc(50% - 15rem));
+        background-color: rgba(65,255,228,0.3);
+        border-radius: 10px;
+        .x-el-form{
+            @include margin-t-r-b-l(5rem, 5rem, 4rem, 5rem);
+            .x-el-item{
+                @include width-height(25rem, null);
+            }
+        }
+    }
+
+</style>
